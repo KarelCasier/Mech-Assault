8dd1ad1235df3c66895c984c91faef17de459f08
KarelCasier <KarelCasier@me.com>
2014-05-07 16:31:10 -0400

    fixed State.h

--- Mech Assault/State.cpp
+++ Mech Assault/State.cpp
@@ -1,9 +1,1 @@
-//
-//  State.cpp
-//  
-//
-//  Created by Karel Casier on 2014-05-07.
-//
-//
-
 #include "State.h"
--- Mech Assault/State.h
+++ Mech Assault/State.h
@@ -1,14 +1,31 @@
-//
-//  State.h
-//  
-//
-//  Created by Karel Casier on 2014-05-07.
-//
-//
+#pragma once
 
-#ifndef ____State__
-#define ____State__
-
-#include <iostream>
-
-#endif /* defined(____State__) */
+class State
+{
+public:
+    typedef std::unique_ptr<State> Ptr;
+    struct Context
+    {
+        
+    };
+    
+public:
+                    State(StateStack& stack, Context context);
+    virtual         ~State();
+    
+    virtual void    draw() = 0;
+    virtual bool    update(sf::Time dt) = 0;
+    virtual bool    handleEvent(const sf::Event& event) = 0;
+    
+protected:
+    void            requestStackPush(States::ID stateID);
+    void            requestStackPop();
+    void            requestStackClear();
+    
+    Context         getContext() const;
+    
+private:
+    StateStack*     mStack;
+    Context         mContext;
+    
+};
